495799e3d5f57cb4108c0343a1513b178acbe607
PigCakee <cw145@mail.ru>
2020-04-26 02:01:26 +0300

    feat: add sign in and log in ability

--- app/src/main/java/com/example/blog/login/LogInViewModel.kt
+++ app/src/main/java/com/example/blog/login/LogInViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.blog.login
 
 import androidx.lifecycle.ViewModel
+import com.example.blog.livedata.mutableLiveData
 
 class LogInViewModel : ViewModel() {
+    val isLoginDialogOpen = mutableLiveData(false)
+
+    val username = mutableLiveData<String>(null)
+
+    fun handleLoginButtonClick() {
+        isLoginDialogOpen.value = true
+    }
+
+    fun handleSuccessfulLogin(username: String) {
+        this.username.value = username
+        isLoginDialogOpen.value = false
+    }
+
+    fun handleCancelledLogin() {
+        isLoginDialogOpen.value = false
+    }
 }
--- app/src/main/java/com/example/blog/signin/SignInActivity.kt
+++ app/src/main/java/com/example/blog/signin/SignInActivity.kt
@@ -2,18 +2,48 @@ package com.example.blog.signin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.blog.R
 import com.example.blog.databinding.ActivitySigninBinding
 import com.example.blog.inflaters.contentView
 import com.example.blog.viewmodel.viewModel
 
-class SignInActivity : AppCompatActivity()  {
+class SignInActivity : AppCompatActivity(), SignInDialogFragment.Listener  {
     private val model by viewModel<SignInViewModel>()
     private val binding by contentView<ActivitySigninBinding>(R.layout.activity_signin)
+
+    private val loginDialogFragment
+        get() = supportFragmentManager.findFragmentByTag("LoginDialog") as? SignInDialogFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.model = model
+
+        model.isLoginDialogOpen.observe(this, Observer {
+            if (it == true) {
+                openLoginDialog()
+            }
+            else {
+                closeLoginDialog()
+            }
+        })
+    }
+
+    private fun openLoginDialog() {
+        loginDialogFragment
+            ?: SignInDialogFragment().show(supportFragmentManager, "LoginDialog")
+    }
+
+    private fun closeLoginDialog() {
+        loginDialogFragment?.dismiss()
+    }
+
+    override fun onLogin(username: String) {
+        model.handleSuccessfulLogin(username)
+    }
+
+    override fun onCancel() {
+        model.handleCancelledLogin()
     }
 }
