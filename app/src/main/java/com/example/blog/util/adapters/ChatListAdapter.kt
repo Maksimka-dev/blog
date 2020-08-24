a5f6adc85ed6b0bf8997f0fbe045a504077e4e27
PigCakee <cw145@mail.ru>
2020-08-24 13:32:16 +0300

    feat: make messages round

--- app/src/main/java/com/example/blog/util/adapters/ChatListAdapter.kt
+++ app/src/main/java/com/example/blog/util/adapters/ChatListAdapter.kt
@@ -2,13 +2,21 @@
 
 package com.example.blog.util.adapters
 
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.withCrossFade
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
+import com.example.blog.R
 import com.example.blog.databinding.MessageBinding
 import com.example.blog.ui.chat.ChatViewModel
 import com.example.blog.util.view.AdapterAvatarData
@@ -43,6 +51,13 @@ class ChatListAdapter(
             Glide.with(messagePic)
                 .asBitmap()
                 .load(picUrl)
+                .transition(withCrossFade())
+                .apply(
+                    RequestOptions().transform(RoundedCorners(15)).error(R.drawable.message_round)
+                        .skipMemoryCache(true).diskCacheStrategy(
+                            DiskCacheStrategy.NONE
+                        )
+                )
                 .into(object : SimpleTarget<Bitmap>() {
                     override fun onResourceReady(
                         resource: Bitmap,
